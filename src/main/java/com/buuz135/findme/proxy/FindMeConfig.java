df76612bf8b0e23c141a159c9cf580b9e5920cab
hnOsmium0001 <36975818+hnOsmium0001@users.noreply.github.com>
2019-09-06 22:46:23 -0700

    Testing items with damage on and off with a config value (default false)

--- src/main/java/com/buuz135/findme/proxy/FindMeConfig.java
+++ src/main/java/com/buuz135/findme/proxy/FindMeConfig.java
@@ -11,10 +11,12 @@ public class FindMeConfig {
 
     public static class Common {
         public ForgeConfigSpec.ConfigValue<Integer> RADIUS_RANGE;
+        public ForgeConfigSpec.ConfigValue<Boolean> IGNORE_ITEM_DAMAGE;
 
         public Common() {
             BUILDER.push("COMMON");
             RADIUS_RANGE = BUILDER.comment("Checking radius centered on the player").defineInRange("RADIUS_RANGE", 8, 0, Integer.MAX_VALUE);
+            IGNORE_ITEM_DAMAGE = BUILDER.comment("Checks item damage or not").define("IGNORE_ITEM_DAMAGE", false);
             BUILDER.pop();
         }
     }
