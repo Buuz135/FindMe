7938130b957355bbe6adc5bcefd1743be97a4ea3
Buuz135 <buuz135@hotmail.com>
2022-01-01 17:56:43 +0100

    Added support for Fabric's Storage api, thanks @Technici4n , closes #24

--- fabric/src/main/java/com/buuz135/fabric/FindMeFabric.java
+++ fabric/src/main/java/com/buuz135/fabric/FindMeFabric.java
@@ -1,13 +1,34 @@
 package com.buuz135.fabric;
 
 import com.buuz135.findme.FindMeMod;
+import com.buuz135.findme.network.PositionRequestMessage;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.impl.transfer.TransferApiImpl;
+import net.fabricmc.fabric.api.transfer.v1.item.ItemStorage;
+import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
+import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
+import net.fabricmc.fabric.api.transfer.v1.storage.StorageView;
+import net.fabricmc.fabric.api.transfer.v1.transaction.Transaction;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.ItemStack;
 
 public class FindMeFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         FindMeMod.init();
-
+        FindMeMod.BLOCK_CHECKERS.add((blockEntity, stack) -> {
+            try (Transaction transaction = Transaction.openOuter()) {
+                for (Direction value : Direction.values()) {
+                    Storage<ItemVariant> storage = ItemStorage.SIDED.find(blockEntity.getLevel(), blockEntity.getBlockPos(), blockEntity.getBlockState(), blockEntity, value);
+                    if (storage == null) continue;
+                    for (StorageView<ItemVariant> itemVariantStorageView : storage.iterable(transaction)) {
+                        ItemStack invStack = itemVariantStorageView.getResource().toStack();
+                        if (!invStack.isEmpty() && PositionRequestMessage.compareItems(stack, invStack)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        });
     }
 }
